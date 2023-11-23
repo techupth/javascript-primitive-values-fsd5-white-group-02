47df8913a96e0c5682c654b3dd8cebb62edc381b
Jaturapat Dusadee <mr.jaturapat.d@gmail.com>
2023-11-23 13:57:03 +0700

    finish exercise

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+
+let totalPrice = 27000;
+let dicountPrice = (25 / 100) * totalPrice;
+let priceAfterDiscount = totalPrice - dicountPrice;
